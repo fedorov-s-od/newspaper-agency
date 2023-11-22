fc9d0cd7a06eeacf5428154036378d4d164c32fd
Serhii Fedorov <fedorov.od.ua@gmail.com>
2023-11-22 23:20:51 +0200

    Add models & creating initial migtrations, add requirements

--- newspaper/models.py
+++ newspaper/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+TOPICS = (
+        ('Politics', 'Politics'),
+        ('Crime', 'Crime'),
+        ('Business', 'Business'),
+        ('Art', 'Art'),
+        ('Trade', 'Trade'),
+        ('Weather', 'Weather'),
+    )
+
+
+class Newspaper(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    published_date = models.DateTimeField()
+    topic = models.ForeignKey('Topic', on_delete=models.CASCADE)
+    publishers = models.ManyToManyField('Redactor', related_name='newspapers')
+
+    def __str__(self):
+        return f"{self.title} ({self.published_date})"
+
+
+class Topic(models.Model):
+    name = models.CharField(max_length=50, choices=TOPICS)
+
+    def __str__(self):
+        return self.name
+
+
+class Redactor(AbstractUser):
+    years_of_experience = models.PositiveSmallIntegerField(default=0)
--- newspaper_agency/settings.py
+++ newspaper_agency/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "newspaper"
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'newspaper_agency.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -99,6 +100,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = "newspaper.Redactor"
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
@@ -116,6 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = (BASE_DIR / "static",)
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
