a4fc1e9136b5ef7a1e89593e812a42a886029690
Serhii Fedorov <fedorov.od.ua@gmail.com>
2023-11-23 11:39:22 +0200

    Update db_data, newspaper list and detail pages

--- newspaper/models.py
+++ newspaper/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils.functional import cached_property
 
 
 class Newspaper(models.Model):
@@ -8,6 +9,14 @@ class Newspaper(models.Model):
     published_date = models.DateTimeField()
     topic = models.ForeignKey('Topic', on_delete=models.CASCADE)
     publishers = models.ManyToManyField('Redactor', related_name='newspapers')
+
+    @cached_property
+    def publishers_to_str(self):
+        return ", ".join(publisher.username for publisher in self.publishers.all())
+
+    @cached_property
+    def content_to_html(self):
+        return "<br />".join(self.content.split("\\n"))
 
     def __str__(self):
         return f"{self.title} ({self.published_date})"
